e8e11238f6e40d18e95ad3c0255df474cf334bdd
Ataxanov <asherzod2197@gmail.com>
2026-04-09 20:41:42 +0500

    Add multiple map examples with lambda functions

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+# 1
+roy = [1, 2, 3, 4, 5]
+print(list(map(lambda x: x * 2, roy)))
 
+# 2
+roy = [10, 20, 30, 40]
+print(list(map(lambda x: x - 5, roy)))
+
+# 3
+roy = ["apple", "banana", "cherry"]
+print(list(map(lambda x: x.upper(), roy)))
+
+# 4
+roy = ["salom", "dunyo", "python"]
+print(list(map(lambda x: len(x), roy)))
+
+# 5
+roy = [3, 6, 9, 12]
+print(list(map(lambda x: x ** 2, roy)))
